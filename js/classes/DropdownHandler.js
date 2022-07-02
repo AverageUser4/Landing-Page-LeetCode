20693d0d655fdfe4e4f5471d43b6a9904fc59b3e
Kacper <kacpersloka@o2.pl>
2022-07-02 21:47:41 +0200

    work on header bar

--- js/classes/DropdownHandler.js
+++ js/classes/DropdownHandler.js
@@ -2,22 +2,60 @@
 
 class DropdownHandler {
 
-  constructor(name) {
-    this.button = document.getElementById(name + 'DropdownButton');
-    this.dropdown = document.getElementById(name + 'Dropdown');
-    this.arrow = document.getElementById(name + 'Arrow')
-    this.button.addEventListener('click', () => this.toggleDropdown());
+  constructor(button_id, dropdown_id, inner_change_id, inner_open, inner_closed, transition_time = 500) {
+    this.button = document.getElementById(button_id);
+    this.dropdown = document.getElementById(dropdown_id);
+    this.dropdown.style.transition = transition_time + 'ms';
+    this.dropdown.classList.add('opacity0');
+    this.transition_time = transition_time;
+    this.change_on = false;
+    if(typeof inner_change_id !== 'undefined') {
+      this.inner_change = document.getElementById(inner_change_id);
+      this.open = inner_open;
+      this.closed = inner_closed;
+      this.change_on = true;
+    }
+
+    this.button.addEventListener('click', (event) => this.toggleDropdown(event));
+    this.dropdown.addEventListener('click', (event) => event.stopPropagation());
   }
 
-  toggleDropdown() {
-    if(window.getComputedStyle(this.dropdown).display === 'none') {
-      this.dropdown.style.display = 'block';
-      this.arrow.innerHTML = '▲';
-    }
-    else {
-      this.dropdown.style.display = 'none';
-      this.arrow.innerHTML = '▼';
-    }
+  toggleDropdown(event) {
+    event.stopPropagation();
+
+    if(window.getComputedStyle(this.dropdown).display === 'none')
+      this.openDropdown();
+    else
+      this.closeDropdown();
   }
+
+  openDropdown() {
+    this.dropdown.style.display = 'block';
+    setTimeout(() => this.makeOpacityOne(), 1);
+    window.addEventListener('click', () => this.closeDropdown());
+    window.addEventListener('resize', () => this.closeDropdown());
+    if(this.change_on)
+      this.inner_change.innerHTML = this.open;
+    this.button.classList.add('active');
+  }
+
+  closeDropdown() {
+    this.dropdown.classList.add('opacity0');
+    setTimeout(() => this.makeDisplayNone(), this.transition_time);
+    window.removeEventListener('click', () => this.closeDropdown());
+    window.removeEventListener('resize', () => this.closeDropdown());
+    if(this.change_on)
+      this.inner_change.innerHTML = this.closed;
+    this.button.classList.remove('active');
+  }
+
+  makeOpacityOne() {
+    this.dropdown.classList.remove('opacity0');
+  }
+
+  makeDisplayNone() {
+    this.dropdown.style.display = 'none';
+  }
+
 
 }
--- js/scripts/main.js
+++ js/scripts/main.js
@@ -1,6 +1,33 @@
 "use strict";
 
-const interview_dropdown_handler = new DropdownHandler('interview');
-const store_dropdown_handler = new DropdownHandler('store');
-const menu_handler = new MenuHandler('mobileMenuButton', 'mobileMenu');
-const bell_handler = new MenuHandler('bell', 'notificationsWrapper');
+const interview_dropdown_handler = new DropdownHandler(
+  'interviewButton', 
+  'interviewDropdown',
+  'interviewArrow',
+  '▲',
+  '▼',
+  0
+);
+const store_dropdown_handler = new DropdownHandler(
+  'storeButton', 
+  'storeDropdown',
+  'storeArrow',
+  '▲',
+  '▼',
+  0
+);
+const menu_handler = new DropdownHandler(
+  'mobileMenuButton',
+  'mobileMenu',
+  'mobileMenuIcon',
+  'X',
+  '☰'
+);
+const bell_handler = new DropdownHandler('bell', 'notificationsWrapper');
+const bell_2_handler = new DropdownHandler('bell2', 'notificationsWrapper');
+const interview_dropdown_desktop = new DropdownHandler(
+  'interviewButtonDesktop', 'interviewDropdownDesktop'
+);
+const store_dropdown_desktop = new DropdownHandler(
+  'storeButtonDesktop', 'storeDropdownDesktop'
+);
